c0fe22084f934ab6d1f6a1c0e733bc5ec32cab3a
Sarthak9149 <168849656+Sarthak9149@users.noreply.github.com>
2025-12-26 13:45:10 +0530

    final commit

---
+++ bubblesort.c
@@ -0,0 +1,12 @@
+void bubbleSort(int arr[], int n) { 
+    int i, j, temp; 
+    for(i = 1; i < n; i++) { 
+        for(j = 0; j < n-i; j++) { 
+            if( arr[j] > arr[j+1]) { // swap the elements 
+                temp = arr[j]; 
+                arr[j] = arr[j+1]; 
+                arr[j+1] = temp; 
+            } 
+        }
+    }
+}
